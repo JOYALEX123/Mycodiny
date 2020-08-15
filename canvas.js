443beccb29d3fd74af955826ab4602c1bba1aa78
JOYALEX123 <65072508+JOYALEX123@users.noreply.github.com>
2020-08-15 16:48:07 +0100

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,16 +1,32 @@
 // canvas.js
 let canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
 let context = canvas.getContext('2d');
-context.fillStyle = 'yellow';
-context.fillRect(10,10, 200, 200);
-context.fillStyle = 'black';
-context.strokeRect(230, 10, 200, 200);
-context.setLineDash([10]);
-context.strokeRect(450, 10, 200, 200);
-context.setLineDash([0]);
-context.strokeStyle = 'red';
-context.strokeRect(10, 230, 200, 200);
-context.fillRect(450, 230, 200, 200);
-context.clearRect(500, 280, 100, 100);
+// Set the starting point to the center of the canvas
+let point = [width / 2, height / 2];
+context.beginPath();
+context.moveTo(point[0], point[1]);
+let i = 1;
+const OFFSET = 10;
+const MARGIN = 5;
+while (
+  point[0] > MARGIN &&
+  point[0] < width - MARGIN &&
+  point[1] > MARGIN &&
+  point[1] < height - MARGIN
+) {
+  point[1] = point[1] - OFFSET * i;
+  context.lineTo(point[0], point[1]);
+  point[0] = point[0] + OFFSET * i;
+  i++;
+  context.lineTo(point[0], point[1]);
+  point[1] = point[1] + OFFSET * i;
+  context.lineTo(point[0], point[1]);
+  point[0] = point[0] - OFFSET * i;
+  i++;
+  context.lineTo(point[0], point[1]);
+}
+context.stroke();
